424d7d486658bc26dda5e3d26aa282d8ad78e8bc
HEYAHONG <2229388563@qq.com>
2025-07-16 16:33:05 +0800

    ifconfig显示标志的具体含义

--- FW/001-base_lowspeed/base_lowspeed/User/hbox_eth.c
+++ FW/001-base_lowspeed/base_lowspeed/User/hbox_eth.c
@@ -231,11 +231,101 @@ static void  hbox_eth_loop(const hruntime_function_t *func)
 }
 HRUNTIME_LOOP_EXPORT(eth,16,hbox_eth_loop,NULL);
 
+static void netif_flags_to_string(uint32_t flags,char *flags_string,size_t max_length)
+{
+    if(flags_string==NULL || max_length == 0)
+    {
+        return;
+    }
+    flags_string[0]='\0';
+    if((flags&NETIF_FLAG_LINK_UP)!=0)
+    {
+        const char * str="RUNNING";
+        if(strlen(str)+strlen(flags_string) < max_length)
+        {
+            strcat(flags_string,str);
+        }
+    }
+    if((flags&NETIF_FLAG_UP)!=0)
+    {
+        const char * str="UP";
+        if(strlen(str)+strlen(flags_string) < (max_length-(strlen(flags_string)>0?1:0)))
+        {
+            if(strlen(flags_string)>0)
+            {
+                strcat(flags_string,",");
+            }
+            strcat(flags_string,str);
+        }
+    }
+    if((flags&NETIF_FLAG_BROADCAST)!=0)
+    {
+        const char * str="BROADCAST";
+        if(strlen(str)+strlen(flags_string) < (max_length-(strlen(flags_string)>0?1:0)))
+        {
+            if(strlen(flags_string)>0)
+            {
+                strcat(flags_string,",");
+            }
+            strcat(flags_string,str);
+        }
+    }
+    if((flags&NETIF_FLAG_ETHARP)!=0)
+    {
+        const char * str="ETHARP";
+        if(strlen(str)+strlen(flags_string) < (max_length-(strlen(flags_string)>0?1:0)))
+        {
+            if(strlen(flags_string)>0)
+            {
+                strcat(flags_string,",");
+            }
+            strcat(flags_string,str);
+        }
+    }
+    if((flags&NETIF_FLAG_ETHERNET)!=0)
+    {
+        const char * str="ETHERNET";
+        if(strlen(str)+strlen(flags_string) < (max_length-(strlen(flags_string)>0?1:0)))
+        {
+            if(strlen(flags_string)>0)
+            {
+                strcat(flags_string,",");
+            }
+            strcat(flags_string,str);
+        }
+    }
+    if((flags&NETIF_FLAG_IGMP)!=0)
+    {
+        const char * str="IGMP";
+        if(strlen(str)+strlen(flags_string) < (max_length-(strlen(flags_string)>0?1:0)))
+        {
+            if(strlen(flags_string)>0)
+            {
+                strcat(flags_string,",");
+            }
+            strcat(flags_string,str);
+        }
+    }
+    if((flags&NETIF_FLAG_MLD6)!=0)
+    {
+        const char * str="MLD6";
+        if(strlen(str)+strlen(flags_string) < (max_length-(strlen(flags_string)>0?1:0)))
+        {
+            if(strlen(flags_string)>0)
+            {
+                strcat(flags_string,",");
+            }
+            strcat(flags_string,str);
+        }
+    }
+}
+
 static int cmd_ifconfig_entry(int argc,const char *argv[])
 {
     hshell_context_t * hshell_ctx=hshell_context_get_from_main_argv(argc,argv);
     if(argc <= 1)
     {
+        hshell_printf(hshell_ctx,"usage: ifconfig interface options\r\n");
         for(uint8_t index=1; index < 254; index++)
         {
             struct netif *interface=netif_get_by_index(index);
@@ -244,10 +334,12 @@ static int cmd_ifconfig_entry(int argc,const char *argv[])
                 {
                     char ifname[NETIF_NAMESIZE]= {0};
                     netif_index_to_name(index,ifname);
+                    char flags_string[96]= {0};
+                    netif_flags_to_string(interface->flags,flags_string,sizeof(flags_string)-1);
 #if LWIP_IPV6 && LWIP_ND6_ALLOW_RA_UPDATES
-                    hshell_printf(hshell_ctx,"%s: flags=%d mtu %d mtu6 %d\r\n",ifname,(int)interface->flags,(int)interface->mtu,(int)interface->mtu6);
+                    hshell_printf(hshell_ctx,"%s: flags=%d<%s> mtu %d mtu6 %d\r\n",ifname,(int)interface->flags,flags_string,(int)interface->mtu,(int)interface->mtu6);
 #else
-                    hshell_printf(hshell_ctx,"%s: flags=%d mtu %d\r\n",(int)interface->flags,(int)interface->mtu);
+                    hshell_printf(hshell_ctx,"%s: flags=%d<%s> mtu %d\r\n",ifname,(int)interface->flags,flags_string,(int)interface->mtu);
 #endif
                 }
 #if             LWIP_IPV4
@@ -289,10 +381,12 @@ static int cmd_ifconfig_entry(int argc,const char *argv[])
                 {
                     char ifname[NETIF_NAMESIZE]= {0};
                     netif_index_to_name(netif_get_index(interface),ifname);
+                    char flags_string[96]= {0};
+                    netif_flags_to_string(interface->flags,flags_string,sizeof(flags_string)-1);
 #if LWIP_IPV6 && LWIP_ND6_ALLOW_RA_UPDATES
-                    hshell_printf(hshell_ctx,"%s: flags=%d mtu %d mtu6 %d\r\n",ifname,(int)interface->flags,(int)interface->mtu,(int)interface->mtu6);
+                    hshell_printf(hshell_ctx,"%s: flags=%d<%s> mtu %d mtu6 %d\r\n",ifname,(int)interface->flags,flags_string,(int)interface->mtu,(int)interface->mtu6);
 #else
-                    hshell_printf(hshell_ctx,"%s: flags=%d mtu %d\r\n",(int)interface->flags,(int)interface->mtu);
+                    hshell_printf(hshell_ctx,"%s: flags=%d<%s> mtu %d\r\n",ifname,(int)interface->flags,flags_string,(int)interface->mtu);
 #endif
                 }
 #if             LWIP_IPV4
